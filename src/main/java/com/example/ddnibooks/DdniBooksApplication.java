3251a269161d7d89105392eada4120959d593b07
ddni <sunjinda88@gmail.com>
2023-11-29 01:59:54 +0900

    init

---
+++ src/main/java/com/example/ddnibooks/DdniBooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddnibooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdniBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdniBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ddnibooks/DdniBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddnibooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdniBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
